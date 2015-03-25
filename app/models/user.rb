229cf4a7c033950327f00b243cdf13f1d1032c37
Chip Neff <chiza007@yahoo.com>
2015-03-24 20:05:20 -0700

    Added Pundit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
   has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  def admin?
+   role == 'admin'
+ end
+ 
+ def moderator?
+   role == 'moderator'
+ end
+ 
 end
