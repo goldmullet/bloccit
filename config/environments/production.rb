9d4e4fb97fb04f04b58ae94e1e7b67e3f9808a7b
Chip Neff <chiza007@yahoo.com>
2015-03-25 23:03:04 -0400

    fixed app name for sending production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # http://bloccit.herokuapp.com
-   config.action_mailer.default_url_options = { host: 'reddit.herokuapp.com' }
+   config.action_mailer.default_url_options = { host: 'chip-reddit.herokuapp.com' }
 end
