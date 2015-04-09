039abf17a180e004494d2e06123a6d03e0682620
Chip Neff <chiza007@yahoo.com>
2015-04-08 21:21:18 -0700

    Complete destroy assignment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,5 @@
 class CommentsController < ApplicationController
   def create
-    # @post = Post.find(params[:post_id])
-    # @comment = current_user.comments.build(comment_params)
-    # @comment.post = @post
-    # authorize @comment
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     @comments = @post.comments
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
  
-  
-  get 'comments/create'
-
   devise_for :users
-     resources :users, only: [:update]
-     resources :topics do
-     resources :posts, except: [:index] do
-     resources :comments, only: [:create, :destroy]
-    end
-   end
+  resources :users, only: [:update]
 
+  resources :topics do
+     resources :posts, except: [:index]
+   end
+ 
+   resources :posts, only: [] do
+     resources :comments, only: [:create, :destroy]
+   end
+  
   get 'about' => 'welcome#about'
 
   root to: "welcome#index"
