01d09d477a0859ba00911b5e28f4ab0936ad6a8e
Chip Neff <chiza007@yahoo.com>
2015-04-18 13:04:40 -0700

    Completed Mocking with Rspec Checkpoint

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,10 +4,12 @@ require 'rails_helper'
    describe "vote methods" do
  
      before do
-       @post = Post.create(title: 'Post title', body: 'Post bodies must be pretty long.')
+       @post = associated_post
        3.times { @post.votes.create(value: 1) }
        2.times { @post.votes.create(value: -1) }
      end
+
+     
  
      describe '#up_votes' do
        it "counts the number of votes with value = 1" do
@@ -25,6 +27,15 @@ require 'rails_helper'
        it "returns the sum of all down and up votes" do
          expect( @post.points ).to eq(1) # 3 - 2
        end
+     end
+   end
+
+   describe '#create_vote' do
+     it "generates an up-vote when explicitly called" do
+       post = associated_post
+       expect( post.up_votes ).to eq(0)
+       post.create_vote
+       expect( post.up_votes ).to eq(1)
      end
    end
  end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,34 +1,28 @@
 require 'rails_helper'
 
 describe Vote do
-  describe "#up_vote?" do
-    it "returns true for an up vote" do
-      v = Vote.new(value: 1)
-      v.up_vote?.should be_true
-    end
-    it "returns false for a down vote" do
-      v = Vote.new(value: -1)
-      v.up_vote?.should be_false
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        
+        up_vote = Vote.new(value: 1)
+        expect(up_vote.valid?).to eq(true)
+
+        down_vote = Vote.new(value: -1)
+        expect(down_vote.valid?).to eq(true)
+
+        invalid_vote = Vote.new(value: 2)
+        expect(invalid_vote.valid?).to eq(false)
+      end
     end
   end
 
-  describe "#down_vote?" do
-    it "returns true for a down vote" do
-      v = Vote.new(value: -1)
-      v.down_vote?.should be_true
-    end
-    it "returns false for an up vote" do
-      v = Vote.new(value: 1)
-      v.down_vote?.should be_false
-    end
-  end
-
-  describe "#update_post" do
-    it "calls 'update_rank' on post" do
-      post = create(:post)
-      post.should respond_to(:update_rank)
-      post.should_receive(:update_rank)
-      Vote.create(value: 1, post: post)
-    end
-  end
+   describe 'after_save' do
+     it "calls `Post#update_rank` after save" do
+       post = associated_post
+       vote = Vote.new(value: 1, post: post)
+       expect(post).to receive(:update_rank)
+       vote.save
+     end
+   end
 end
