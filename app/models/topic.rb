450140cb04f53fdfa3ac3e5886d1722602716b4c
Chip Neff <chiza007@yahoo.com>
2015-04-29 20:58:51 -0700

    Fixed topic.rb

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,9 +1,9 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
-  scope :public, -> { where(public: true) }
-  scope :private, -> { where(public: false) }
-  scope :visible_to, -> (user) { user ? all : public }
+   scope :visible_to, -> (user) { user ? all : where(public: true) }
+
+   validates :name, length: { minimum: 5 }, presence: true
  
 
 end
