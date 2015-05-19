af1226da18b2988bf14cd1fa8cd86777e06b8674
Chip Neff <chiza007@yahoo.com>
2015-05-19 13:37:32 -0700

    Fixed bugs of Popular Posts

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,71 +1,59 @@
-class TopicsController < ApplicationController
-  def index
-    #@topics = Topic.all
-    #@topics = Topic.paginate(page: params[:page], per_page: 10)
+ class TopicsController < ApplicationController
+   def index
     @topics = Topic.visible_to(current_user).paginate(page: params[:page], per_page: 10)
     authorize @topics
   end
 
-  def new
-    @topic = Topic.new
+  def show
+    @topic= Topic.find(params[:id])
     authorize @topic
+    @posts=@topic.posts.includes(:user).includes(:comments).paginate(page: params[:page], per_page: 10)
   end
 
- def show
-     @topic = Topic.find(params[:id])
-    
+  def new
+    @topic=Topic.new
+      authorize @topic
+  end
+ 
+ def create
+      @topic= Topic.new(params.require(:topic).permit(:name,:description,:public))
      authorize @topic
-     @posts = @topic.posts.includes(:user).includes(:comments).paginate(page: params[:page], per_page: 10)
+     if @topic.save
+       flash[:notice] = "Topic was saved."
+       redirect_to @topic
+     else
+       flash[:error] = "There was an error saving the topic. Please try again."
+       render :new
+     end
    end
 
   def edit
     @topic = Topic.find(params[:id])
-    @topic.save
-    authorize @topic
+      authorize @topic
   end
-  
-  def create
-     #@topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
-     @topic = Topic.new(topic_params)
-     #@topic = current_user.Topics.build(topic_params)
-     authorize @topic
-     if @topic.save
-       redirect_to @topic, notice: "Topic was saved successfully."
-     else
-       flash[:error] = "Error creating topic. Please try again."
-       render :new
-     end
-   end
- 
-   def update
-     @topic = Topic.find(params[:id])
-     #@topic = Topic.new(topic_params)
-     authorize @topic
-     if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
+
+def update
+     @topic= Topic.find(params[:id])
+      authorize @topic
+     if @topic.update_attributes(params.require(:topic).permit(:name,:description,:public))
+       flash[:notice] = "Topic was updated."
        redirect_to @topic
      else
-       flash[:error] = "Error saving topic. Please try again."
+       flash[:error] = "There was an error saving the topic Please try again."
        render :edit
      end
    end
-
   def destroy
      @topic = Topic.find(params[:id])
+     name = @topic.name
  
      authorize @topic
      if @topic.destroy
-       flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+       flash[:notice] = "\"#{name}\" was deleted successfully."
        redirect_to topics_path
      else
        flash[:error] = "There was an error deleting the topic."
        render :show
      end
-  end
-  
-  private
-
-  def topic_params
-    params.require(:topic).permit(:name, :description, :public)
-  end
-  
-end
+   end
+ end
