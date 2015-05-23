0c2e76e8e48e81e0f5a8e6147d6f62a98087375d
Chip Neff <chiza007@yahoo.com>
2015-05-23 13:02:26 -0700

    Reversed defualt Scope

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,15 +3,23 @@ class Comment < ActiveRecord::Base
   belongs_to :user
 
   validates :body, length: { minimum: 5 }, presence: true
-  validates :user_id, presence: true
-  
-  after_create :send_favorite_emails
+  validates :user, presence: true
+
+   after_create :send_favorite_emails
+
+   default_scope { order('updated_at ASC') }
  
    private
  
-   def send_favorite_emails
-     post.favorites.each do |favorite|
-       FavoriteMailer.new_comment(favorite.user, post, self).deliver_now
-     end
+  def send_favorite_emails
+    post.favorites.each do |favorite|
+       if should_receive_update_for?(favorite)
+        FavoriteMailer.new_comment(favorite.user, post, self).deliver
+      end
+    end
+  end
+ 
+   def should_receive_update_for?(favorite)
+     user_id != favorite.user_id && favorite.user.email_favorites?
    end
 end
